76b489f2319755a0f806b52d86d553efb453a5f4
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2016-10-25 17:45:32 +0300

    Initial implementation

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = (attributes) => attributes
+const gather = (prefix, updates) =>
+    Object.keys(updates).map(key =>
+        typeof updates[key] === 'object'
+            ? gather(`${prefix}${key}.`, updates[key])
+            : `${prefix}${key} = ${updates[key]}`
+    ).join(', ')
+
+module.exports = (updates) =>
+    'SET ' + gather('', updates)
