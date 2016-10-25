8adfe53732c59fe29923f85d0098cda743eac8cd
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2016-10-25 17:23:10 +0300

    Add a complex test case

--- test.js
+++ test.js
@@ -1,8 +1,20 @@
 const test = require('tape')
 const merge = require('./index')
 
-test('first', t => {
+test('SET', t => {
     t.plan(1)
 
     t.equal(merge({x: 42}), 'SET x = 42')
+})
+
+test.skip('ADD', t => {
+})
+
+test('Combination', t => {
+    t.plan(1)
+
+    t.equal(
+        merge({x: {y: [1, 2], z: 'x'}, q: 10}), 
+        'SET x.z = "x", q = 10 ADD x.y 1, x.y 2'
+    )
 })
